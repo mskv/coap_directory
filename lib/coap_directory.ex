02e253461479c0eb2e4f8d752d2c143f70a5e697
Michal Podworny <michal.podworny@monterail.com>
2016-08-18 22:18:51 +0200

    Perform actual request in observation responder

--- lib/coap_directory.ex
+++ lib/coap_directory.ex
@@ -5,6 +5,7 @@ defmodule CoapDirectory do
     dispatch = :cowboy_router.compile([{:_, [{"/[...]", CoapDirectory.HttpRequestHandler, []}]}])
     {:ok, _} = :cowboy.start_http(:http, 100, [port: 8080], [env: [dispatch: dispatch]])
     CoapDirectory.ResourceRegistry.start
+    CoapDirectory.ObservationResponder.start
     CoapDirectory.Supervisor.start_link
   end
 end
--- lib/coap_directory/observation_responder.ex
+++ lib/coap_directory/observation_responder.ex
@@ -1,5 +1,6 @@
 defmodule CoapDirectory.ObservationResponder do
   use GenServer
+  use HTTPoison.Base
 
   def start_link(callback_url) do
     GenServer.start_link(__MODULE__, [callback_url], [])
@@ -11,7 +12,17 @@ defmodule CoapDirectory.ObservationResponder do
     {:ok, callback_url}
   end
 
-  def handle_info(_msg, callback_url) do
+  def handle_info(resource_observation, callback_url) do
+    [resource_name, resource_state] = String.split(resource_observation, " ")
+    post!(callback_url, %{resource: %{name: resource_name, state: resource_state}})
     {:noreply, callback_url}
+  end
+
+  defp process_request_body(body) do
+    Poison.encode!(body)
+  end
+
+  defp process_request_headers(headers) do
+    [{"content-type", "application/json"} | headers]
   end
 end
